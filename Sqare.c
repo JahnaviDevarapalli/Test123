6460782a1638ac696ebefa932607e3bbd9903bd6
Jahnavi Devarapalli <2020wb86396@wilp.bits-pilani.ac.in>
2024-04-20 22:20:50 +0530

    new file in main branch

---
+++ Sqare.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+float square(float num);
+
+void main()
+{
+    float num, result;
+    printf("Enter number: ");
+    scanf("%f", &num);
+
+    result = square(num);
+    printf("Square of %0.2f is %0.2f", num, result);
+}
+
+float square(float num)
+{
+    return num * num;
+}
